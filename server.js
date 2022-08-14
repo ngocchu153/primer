ee7c494a4308693e50d6300c18573e93ccdd42c7
ngoc.chu <19349177+ngocchu153@users.noreply.github.com>
2022-08-14 14:39:55 +0700

    feat: add rate limiting

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const next = require('next');
+const rateLimit = require('express-rate-limit');
+
+const MAX_REQUESTS = 100;
+const RATE_LIMIT_TIMEFRAME = 10;
+
+const apiLimiter = rateLimit({
+  windowMs: RATE_LIMIT_TIMEFRAME * 60 * 1000, // 10 minutes
+  max: MAX_REQUESTS, // Limit each IP to 100 requests per windowsMs
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: async () => {
+    return ({ message: `You can only make ${MAX_REQUESTS} requests every ${RATE_LIMIT_TIMEFRAME} minutes.` })
+  },
+})
+
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const hostname = 'localhost'
+const dev = process.env.NODE_ENV !== 'production';
+
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.use('/api/v1', apiLimiter);
+
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (error) => {
+    if (error) throw error;
+    console.info(`> Ready on http://${hostname}:${port}`);
+  });
+});
