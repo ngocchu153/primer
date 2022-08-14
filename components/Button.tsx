d0f3a3485cba227fc7a9bc7f78248b8f69f6cb9e
ngoc.chu <19349177+ngocchu153@users.noreply.github.com>
2022-08-15 01:04:29 +0700

    style: Button css

--- components/Button.tsx
+++ components/Button.tsx
@@ -19,7 +19,7 @@ function Button({
     <button
       className={`rounded border border-transparent bg-gradient-to-r from-indigo-500 to-violet-500 px-6 py-2 text-white
       hover:opacity-90 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:bg-none disabled:text-gray-300 ${
-        loading || 'opacity-50'
+        loading ? 'opacity-50' : ''
       } ${className ?? ''}`}
       disabled={disabled}
       type={type}
